d79023b0b20e18e63dadf4d3a53fd16e5456747f
chuhx <chuhx1024@126.com>
2021-03-11 18:49:34 +0800

    Feat: Use css module by scss

--- src/views/example/index.jsx
+++ src/views/example/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import store from '../../store'
+import styles from './index.module.scss'
 export default class Example extends React.Component {
     state = store.getState()
     render () {
         return (
-            <>
-                <div>{this.state.inputVal}</div>
-            </>
+            <section className={styles.exampleContainer}>
+                <div className={styles.mainTitle}>{this.state.inputVal}</div>
+            </section>
         )
     }
 }
